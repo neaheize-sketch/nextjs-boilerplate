a44f8754c6635b0f89170549592b27d1cb5419db
neaheize-sketch <neaheize@gmail.com>
2025-12-19 16:24:17 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             ぱるせぶんすたー.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            みてみてコーディングしたｗ Head over to{" "}
+            みてみてコーディングしたｗ {" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -31,7 +31,7 @@ export default function Home() {
             >
               それはぱるやんセブン
             </a>{" "}
-            center.
+            
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
