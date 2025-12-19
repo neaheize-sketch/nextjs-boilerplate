e2cb0682ccd7c35655097fa19ef489905555cccc
neaheize-sketch <neaheize@gmail.com>
2025-12-19 16:22:36 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,19 +17,19 @@ export default function Home() {
             ぱるせぶんすたー.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            みてみてコーディングしたｗ Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
+              青い髪の毛
             </a>{" "}
-            or the{" "}
+            白色のマフラー{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+              それはぱるやんセブン
             </a>{" "}
             center.
           </p>
